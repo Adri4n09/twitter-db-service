6b9d2994c747fc91d2795e4b17d406881238b8f3
Adri4n09 <imihai.adrian@gmail.com>
2018-03-25 12:45:35 +0300

    First commit

---
+++ src/main/java/com/adrian/twitter/TwitterDbServiceApplication.java
@@ -0,0 +1,12 @@
+package com.adrian.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterDbServiceApplication.class, args);
+	}
+}
